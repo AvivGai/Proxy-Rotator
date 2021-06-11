2bac15e6010b32b9a3c018d88db5d6cb9e2c67db
AvivGai <39157293+AvivGai@users.noreply.github.com>
2021-06-11 14:53:47 +0300

    Add files via upload
    configuration file

---
+++ configuration.py
@@ -0,0 +1,8 @@
+COUNTRIES = ['us', 'uk']
+COUNTRY_PROXY_ADDRESSES_KEY_PREFIX = "country_proxy_addresses"
+INVALID_PROXY_KEY = "invalid_proxies"
+INVALID_TIME_IN_SEC = 10
+FLASK_HOST = "localhost"
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+DATA_FILE_NAME = "MOCK_PROXIES_DATA_7.json"
